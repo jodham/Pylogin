cf1b2823b52e9e4096ccc0af8cec919096277548
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2021-01-14 01:20:48 +0300

    update

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -11,9 +12,8 @@ mycur = mydb.cursor()
 #mycur.execute("CREATE DATABASE mydatabase1")
 #mycur.execute("CREATE TABLE logindetails(id Integer PRIMARY KEY AUTO_INCREMENT,username VARCHAR(255),password VARCHAR(255))")
 
-
-
 class Login:
+    global Login
     def __init__(self):
         self.loginWindow = Tk()
         self.loginWindow.title("login")
@@ -32,6 +32,7 @@ class Login:
         self.button1.place(x=90, y=170)
         self.button2 = Button(self.loginWindow, text="Forgot password", font="times 10")
         self.button2.place(x=90,y=220)
+
     def run(self):
         self.loginWindow.mainloop()
 
@@ -44,13 +45,20 @@ class Register:
         self.label1.place(x=80,y=40)
         self.label2 = Label(self.registerWindow, text="Username", font="times 15 bold")
         self.label2.place(x=80,y=80)
-        self.e1 = Entry(self.registerWindow)
-        self.e1.place(x=80, y=110)
+        self.username_ent = Entry(self.registerWindow)
+        self.username_ent.place(x=80, y=110)
         self.label3 = Label(self.registerWindow, text="Password", font="times 15 bold")
         self.label3.place(x=80,y=130)
-        self.e2 = Entry(self.registerWindow, show='*')
-        self.e2.place(x=80, y=160)
-        self.button1 = Button(self.registerWindow, text="Register",font="times 13 bold")
+        self.pass_ent = Entry(self.registerWindow, show='*')
+        self.pass_ent.place(x=80, y=160)
+        self.button1 = Button(self.registerWindow, text="Register",font="times 13 bold",command=self.addUser)
         self.button1.place(x=90, y=190)
+
+    def addUser(self):
+        self.username = self.username_ent.get()
+        self.password = self.pass_ent.get()
+
+        if self.username =="":
+            tkinter.messagebox.showinfo("warning","please fill info")
     def run(self):
         self.registerWindow.mainloop()
