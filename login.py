f40a7f60898395c9f1f08237ec8e94af7fead424
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2021-01-14 00:37:36 +0300

    update

--- login.py
+++ login.py
@@ -1,4 +1,16 @@
 from tkinter import *
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="mydatabase1"
+)
+mycur = mydb.cursor()
+#mycur.execute("CREATE DATABASE mydatabase1")
+#mycur.execute("CREATE TABLE logindetails(id Integer PRIMARY KEY AUTO_INCREMENT,username VARCHAR(255),password VARCHAR(255))")
+
 
 
 class Login:
